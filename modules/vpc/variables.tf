b7f87433ba0a70ef1f9165534078425ae3aa224c
Cbroaden <167448556+daczar123@users.noreply.github.com>
2025-12-13 15:42:42 -0800

    Add VPC module variables

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,23 @@
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  type        = list(string)
+  description = "List of public subnet CIDR blocks"
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  type        = list(string)
+  description = "List of private subnet CIDR blocks"
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Map of tags to apply to resources"
+  default     = {}
+}
